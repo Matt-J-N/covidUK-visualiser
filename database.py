b50c954f98f047d608e244881048c8fe3e28506c
Matt Neersoo <66684091+Matt-J-N@users.noreply.github.com>
2021-04-06 13:52:03 +0100

    Add basis for graphing from MongoDB

--- database.py
+++ database.py
@@ -1,5 +1,3 @@
-import pandas as pd
-#import pymongo
 from uk_covid19 import Cov19API
 from pymongo import MongoClient
 
@@ -8,10 +6,10 @@ client = MongoClient("mongodb+srv://matt:fjTrmxLnqiSqKi70@cluster0.llzxg.mongodb
 #Database
 db = client["covid_data"]
 #Collection
-collection = db["vacc"]
+collection = db["other"]
 
 #Define metric structures
-#Metrics for main
+#Metrics for main - Cases/Deaths
 metrics = {
         "date": "date",
         "areaName": "areaName",
@@ -23,25 +21,24 @@ metrics = {
         "cumAdmissions" : "cumAdmissions"
     }
 
-#Metrics for vacc
+#Metrics for other - Vaccinations/Tests
 secondary_metrics = {
         "date": "date",
         "areaName": "areaName",
         "areaCode": "areaCode",
         "cumPeopleVaccinatedFirstDoseByPublishDate": "cumPeopleVaccinatedFirstDoseByPublishDate",
         "cumPeopleVaccinatedSecondDoseByPublishDate": "cumPeopleVaccinatedSecondDoseByPublishDate",
-        "cumPeopleVaccinatedCompleteByPublishDate": "cumPeopleVaccinatedCompleteByPublishDate"
+        "cumPeopleVaccinatedCompleteByPublishDate": "cumPeopleVaccinatedCompleteByPublishDate",
+        "cumVirusTests": "cumVirusTests"
     }
 
 def data_loading(this_area, this_metric):
     area_type = [this_area]
     api = Cov19API(filters=area_type, structure= this_metric)
     data = api.get_json()
-    #data['date'] = pd.to_datetime(data['date'])
-    #data.sort_values('date', ascending = True, inplace = True, ignore_index = True)
     return data
 
-data_init = data_loading('areaType=', )
+data_init = data_loading('areaType=nation', secondary_metrics)
 collection.insert_many(data_init['data'])
 
 
--- plot.py
+++ plot.py
@@ -1,20 +1,33 @@
 import plotly.express as px
 import plotly.io as pio
 import pandas as pd
+from database import client
+
 #INTERACTIVE DISPLAY IN BROWSER
 pio.renderers.default='browser'
 
 #DISPLAY IN SPYDER
 #pio.renderers.default='svg'
 
-#def set_metrics():
+db = client["covid_data"]
+main_coll = db["main"]
+other_coll = db["other"]
+
+test_FIND = main_coll.find({"areaName":"London"})
+for test in test_FIND:  
+    print(test)
     
+data = pd.DataFrame(list(main_coll.find({})))
+data['date'] = pd.to_datetime(data['date'])
+data.sort_values('date', ascending = True, inplace = True, ignore_index = True)
+
+#def set_metrics():    
 def test_out():
-    this_data = get_data('areaType=region')
-    print(this_data)  
+    #this_data = get_data('areaType=region')
+    #print(this_data)  
     
     test_bar = px.bar(
-                data_frame = this_data, 
+                data_frame = data, 
                 x = 'areaName', 
                 y = 'cumDeathsByPublishDate', 
                 color = 'areaName', 
